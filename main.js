f4d7dbddb976503351cefbaf95c21fc8f4a0967b
carl-thilander <carl.thilander@medieinstitutet.se>
2024-11-06 10:59:18 +0100

    finished frontpage

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-=======
-window.addEventListener('DOMContentLoaded',main)
 
-function main() {
-
-}
-
-
-
->>>>>>> be4fac438d7fd4c163941988640ba5c84ec1cc2f
-function navigate(pageId) {
-    var pages = document.querySelectorAll('.page');
-    pages.forEach(page=> page.classList.remove('visible'));
-
-    document.getElementById(pageId).classList.add('visible');
-}   /*Byter sida vid knapptryck till vald sida*/
 
 
 
